7ed3eaf7ea715decd3abe5d0c0c3f72590b40446
herleen <33649578+mmadsoup@users.noreply.github.com>
2025-03-31 12:23:36 -0700

    Removed comments in player movement class

--- Floreo-Interview-Demo/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Floreo-Interview-Demo/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -7,9 +7,6 @@ using UnityEngine.InputSystem;
 using StarterAssets.Player.Camera;
 #endif
 
-/* Note: animations are called via the controller for both the character and capsule using animator null checks
- */
-
 namespace StarterAssets.Player.Movement
 {
     [RequireComponent(typeof(CharacterController))]
@@ -49,7 +46,6 @@ namespace StarterAssets.Player.Movement
 
         private void Awake()
         {
-            // get a reference to our main camera
             if (_mainCamera == null)
             {
                 _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
@@ -71,7 +67,6 @@ namespace StarterAssets.Player.Movement
 #else       
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
-            // reset our timeouts on start
             jumpTimeoutDelta = PlayerComponents.JumpTimeout;
             fallTimeoutDelta = PlayerComponents.FallTimeout;
         }
@@ -92,44 +87,32 @@ namespace StarterAssets.Player.Movement
         
         protected override void GroundedCheck()
         {
-            // set sphere position, with offset
             Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - PlayerComponents.GroundedOffset,
                 transform.position.z);
             PlayerComponents.Grounded = Physics.CheckSphere(spherePosition, PlayerComponents.GroundedRadius, PlayerComponents.GroundLayers,
                 QueryTriggerInteraction.Ignore);
 
-            // update animator if using characte(r
             _playerAnimator.PlayGroundedAnimation();
         }
 
 
         protected override void Move()
         {
-            // set target speed based on move speed, sprint speed and if sprint is pressed
             float targetSpeed = _input.sprint ? PlayerComponents.SprintSpeed : PlayerComponents.MoveSpeed;
 
-            // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
-
-            // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-            // if there is no input, set the target speed to 0
             if (_input.move == Vector2.zero) targetSpeed = 0.0f;
 
-            // a reference to the players current horizontal velocity
             float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
 
             float speedOffset = 0.1f;
             float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
 
-            // accelerate or decelerate to target speed
             if (currentHorizontalSpeed < targetSpeed - speedOffset ||
                 currentHorizontalSpeed > targetSpeed + speedOffset)
             {
-                // creates curved result rather than a linear one giving a more organic speed change
-                // note T in Lerp is clamped, so we don't need to clamp our speed
                 speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
                     Time.deltaTime * PlayerComponents.SpeedChangeRate);
 
-                // round speed to 3 decimal places
                 speed = Mathf.Round(speed * 1000f) / 1000f;
             }
             else
@@ -140,11 +123,8 @@ namespace StarterAssets.Player.Movement
             _playerAnimator.AnimationBlend = Mathf.Lerp(_playerAnimator.AnimationBlend, targetSpeed, Time.deltaTime * PlayerComponents.SpeedChangeRate);
             if (_playerAnimator.AnimationBlend < 0.01f) _playerAnimator.AnimationBlend = 0f;
 
-            // normalise input direction
             Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
 
-            // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-            // if there is a move input rotate player when the player is moving
             if (_input.move != Vector2.zero)
             {
                 targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
@@ -152,14 +132,12 @@ namespace StarterAssets.Player.Movement
                 float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity,
                     PlayerComponents.RotationSmoothTime);
 
-                // rotate to face input direction relative to camera position
                 transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
             }
 
 
             Vector3 targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
 
-            // move the player
             _controller.Move(targetDirection.normalized * (speed * Time.deltaTime) +
                              new Vector3(0.0f, verticalVelocity, 0.0f) * Time.deltaTime);
 
@@ -170,27 +148,22 @@ namespace StarterAssets.Player.Movement
         {
             if (PlayerComponents.Grounded)
             {
-                // reset the fall timeout timer
                 fallTimeoutDelta = PlayerComponents.FallTimeout;
 
                 _playerAnimator.PlayJumpAndFallAnimation();
 
-                // stop our velocity dropping infinitely when grounded
                 if (verticalVelocity < 0.0f)
                 {
                     verticalVelocity = -2f;
                 }
 
-                // Jump
                 if (_input.jump && jumpTimeoutDelta <= 0.0f)
                 {
-                    // the square root of H * -2 * G = how much velocity needed to reach desired height
                     verticalVelocity = Mathf.Sqrt(PlayerComponents.JumpHeight * -2f * PlayerComponents.Gravity);
 
                     _playerAnimator.PlayJumpAnimation();
                 }
 
-                // jump timeout
                 if (jumpTimeoutDelta >= 0.0f)
                 {
                     jumpTimeoutDelta -= Time.deltaTime;
@@ -198,10 +171,8 @@ namespace StarterAssets.Player.Movement
             }
             else
             {
-                // reset the jump timeout timer
                 jumpTimeoutDelta = PlayerComponents.JumpTimeout;
 
-                // fall timeout
                 if (fallTimeoutDelta >= 0.0f)
                 {
                     fallTimeoutDelta -= Time.deltaTime;
@@ -211,11 +182,9 @@ namespace StarterAssets.Player.Movement
                     _playerAnimator.PlayFallAnimation();
                 }
 
-                // if we are not grounded, do not jump
                 _input.jump = false;
             }
 
-            // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
             if (verticalVelocity < terminalVelocity)
             {
                 verticalVelocity += PlayerComponents.Gravity * Time.deltaTime;
