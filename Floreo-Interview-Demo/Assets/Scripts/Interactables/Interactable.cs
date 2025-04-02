ace508870e1ed7cbea9e5154e0e5fc3f909f0f76
herleen <33649578+mmadsoup@users.noreply.github.com>
2025-04-02 01:39:01 -0700

    Debugging async loading

--- Floreo-Interview-Demo/Assets/Scripts/Interactables/Interactable.cs
+++ Floreo-Interview-Demo/Assets/Scripts/Interactables/Interactable.cs
@@ -1,49 +1,69 @@
 using StarterAssets.AdrressableObjects;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
-public class Interactable : MonoBehaviour
+using UnityEngine.SceneManagement;
+namespace StarterAssets.Interactable
 {
-    [SerializeField] private string _addressablePath;
-    [SerializeField] private string _singlePlayerScene;
-    [SerializeField] private string _multiplayerScene;
-    private AddressableInstantiator _addressableInstantiator;
-    private GameObject _mainCamera;
-    private GameObject _uiEvents;
-    private GameObject _environment;
-    private AsyncOperationHandle<SceneInstance> _handle;
-
-    void Awake()
+    public class Interactable : MonoBehaviour
     {
-        var add = GameObject.FindGameObjectWithTag("AddressableInstantiator");
-        _addressableInstantiator = add.GetComponent<AddressableInstantiator>();
+        [SerializeField] private string _addressablePath;
+        [SerializeField] private string _singlePlayerScene;
+        [SerializeField] private string _multiplayerScene;
+        private AddressableInstantiator _addressableInstantiator;
+        private GameObject _mainCamera;
+        private GameObject _uiEvents;
+        private GameObject _environment;
+        private AsyncOperationHandle<SceneInstance> _handle;
+        private bool _spawned = false;
 
-         _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
-         _uiEvents = GameObject.FindGameObjectWithTag("UIEvents");
-         _environment = GameObject.FindGameObjectWithTag("Environment");        
+        void Awake()
+        {
+            var add = GameObject.FindGameObjectWithTag("AddressableInstantiator");
+            _addressableInstantiator = add.GetComponent<AddressableInstantiator>();
+
+            _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+            _uiEvents = GameObject.FindGameObjectWithTag("UIEvents");
+            _environment = GameObject.FindGameObjectWithTag("Environment");        
+        }
+
+        private AsyncOperationHandle<SceneInstance> GetHandle(string address)
+        {
+            return Addressables.LoadSceneAsync(address);
+        }
+        void OnTriggerEnter(Collider other)
+        {
+            if (!_spawned) {
+            _handle = GetHandle(name);
+            Addressables.LoadSceneAsync(name);
+            _addressableInstantiator.LoadSceneAdditive(_addressablePath);
+
+            Scene scene = SceneManager.GetSceneByName(_singlePlayerScene).IsValid() ? SceneManager.GetSceneByName(_singlePlayerScene) : SceneManager.GetSceneByName(_multiplayerScene);
+            
+            if (scene.IsValid())
+            {
+                foreach (GameObject obj in scene.GetRootGameObjects())
+                {
+
+                        obj.SetActive(false);
+                    
+                }
+            }
+
+            _uiEvents.SetActive(false);
+            _mainCamera.SetActive(false);
+            }
+            _spawned = true;
+            //_environment.SetActive(false);
+        }
+        void OnTriggerExit(Collider other)
+        {
+            _uiEvents.SetActive(true);
+            _mainCamera.SetActive(true);
+            //_environment.SetActive(true);
+            _addressableInstantiator.UnloadSceneAdditive(_handle);
+        }
     }
 
-    private AsyncOperationHandle<SceneInstance> GetHandle(string address)
-    {
-        return Addressables.LoadSceneAsync(address);
-    }
-    void OnTriggerEnter(Collider other)
-    {
-
-        _handle = GetHandle(name);
-        Addressables.LoadSceneAsync(name);
-        _addressableInstantiator.LoadSceneAdditive(_addressablePath);
-        _uiEvents.SetActive(false);
-        _mainCamera.SetActive(false);
-        _environment.SetActive(false);
-    }
-    void OnTriggerExit(Collider other)
-    {
-        _uiEvents.SetActive(true);
-        _mainCamera.SetActive(true);
-        _environment.SetActive(true);
-        _addressableInstantiator.UnloadSceneAdditive(_handle);
-    }
 }
